bd90c9cda32939c7bcb16cb62ff6fd245c8d72bb
Moritz Makowski <makowskimoritz@gmail.com>
2019-10-29 18:26:08 +0100

    Updated tutorial 03

--- tutorial-02/example_2_4_division_remainder.c
+++ tutorial-02/example_2_4_division_remainder.c
@@ -2,14 +2,10 @@
 #include <stdio.h>
 
 int main() {
-    printf("\n%d", 12 % 3);
-    printf("\n%d", 11 % 3);
-    printf("\n%d", 10 % 3);
-    printf("\n%d",  9 % 3);
-    printf("\n%d",  8 % 3);
-    printf("\n%d",  7 % 3);
-    printf("\n%d",  6 % 3);
-    printf("\n%d\n",  5 % 3);
+    int count = 10;
+    while (count >= 1) {
+        printf("Number is %d\n", count--);
 
-    return 0;
+    }
+
 }
